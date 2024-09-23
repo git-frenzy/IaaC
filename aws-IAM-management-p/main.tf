6e565a80405a231f96a2d82f40dc672e236613c5
Kanishk <kanishksuri29@gmail.com>
2024-09-24 00:57:42 +0530

    update

---
+++ aws-IAM-management-p/main.tf
@@ -0,0 +1,14 @@
+locals {
+  users_data = yamldecode(file("./users.yaml")).users
+}
+
+output "output" {
+  value = local.users_data[*].username
+}
+
+// creating users
+
+resource "aws_iam_user" "main" {
+    name = 
+  
+}
